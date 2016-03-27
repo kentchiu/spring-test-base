f0fbaa18bb357fd90c4f7f637aaf4fd755dc168e
Kent <kent.cwg@gmail.com>
2016-03-27 21:51:12 +0800

    release 1.1.0

---
+++ src/main/java/com/kentchiu/spring/base/web/ApiDocInfoCollector.java
@@ -0,0 +1,121 @@
+package com.kentchiu.spring.base.web;
+
+import org.apache.commons.collections.map.HashedMap;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.restdocs.RestDocumentationContext;
+import org.springframework.restdocs.operation.Operation;
+import org.springframework.restdocs.snippet.Snippet;
+import org.springframework.restdocs.snippet.WriterResolver;
+import org.springframework.restdocs.templates.TemplateEngine;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class ApiDocInfoCollector implements Snippet {
+
+    private Operation operation;
+    private List<Section> sections = new ArrayList<>();
+
+    @Override
+    public void document(Operation operation) throws IOException {
+        RestDocumentationContext context = (RestDocumentationContext) operation.getAttributes().get(RestDocumentationContext.class.getName());
+        this.operation = operation;
+        String uri = (String) operation.getAttributes().get("org.springframework.web.servlet.HandlerMapping.bestMatchingPattern");
+        Section section = sections.stream().filter(s -> StringUtils.equals(s.getUri(), uri)).findAny().orElseGet(() -> {
+            Section s = new Section();
+            s.setUri(uri);
+            sections.add(s);
+            return s;
+        });
+        String include = "include::{snippets}/" + context.getTestClass().getSimpleName() + "/" + context.getTestMethodName() + "/ApiDoc.adoc[]";
+        Api api = new Api(operation.getRequest().getMethod().name(), include);
+        section.api.add(api);
+
+    }
+
+    public void writeAndFlush() throws IOException {
+        RestDocumentationContext context = (RestDocumentationContext) operation
+                .getAttributes().get(RestDocumentationContext.class.getName());
+        WriterResolver writerResolver = (WriterResolver) operation.getAttributes().get(
+                WriterResolver.class.getName());
+        String snippetName = "RestDoc";
+        try (Writer writer = writerResolver.resolve("{ClassName}", snippetName, context)) {
+            Map<String, Object> model = createModel(operation);
+            TemplateEngine templateEngine = (TemplateEngine) operation.getAttributes().get(TemplateEngine.class.getName());
+            writer.append(templateEngine.compileTemplate(snippetName).render(model));
+        }
+        sections.clear();
+    }
+
+    private Map<String, Object> createModel(Operation operation) {
+        Map<String, Object> result = new HashedMap();
+        List<Section> sortedSessions = sections.stream().sorted((o1, o2) -> o1.uri.length() > o2.getUri().length() ? 1 : -1).collect(Collectors.toList());
+        result.put("section", sortedSessions);
+        return result;
+    }
+
+    class Section {
+        private String uri;
+        private List<Api> api = new ArrayList<>();
+
+        public String getUri() {
+            return uri;
+        }
+
+        public void setUri(String uri) {
+            this.uri = uri;
+        }
+
+        public List<Api> getApi() {
+            return api;
+        }
+
+        public void setApi(List<Api> api) {
+            this.api = api;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Section section = (Section) o;
+            return Objects.equals(uri, section.uri);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(uri);
+        }
+    }
+
+    class Api {
+        private String httpMethod;
+        private String include;
+
+        public Api(String httpMethod, String include) {
+            this.httpMethod = httpMethod;
+            this.include = include;
+        }
+
+        public String getHttpMethod() {
+            return httpMethod;
+        }
+
+        public void setHttpMethod(String httpMethod) {
+            this.httpMethod = httpMethod;
+        }
+
+        public String getInclude() {
+            return include;
+        }
+
+        public void setInclude(String include) {
+            this.include = include;
+        }
+    }
+}
---
+++ src/main/java/com/kentchiu/spring/base/web/AttributeSnippet.java
@@ -0,0 +1,241 @@
+package com.kentchiu.spring.base.web;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+import com.kentchiu.spring.attribute.Attribute;
+import com.kentchiu.spring.attribute.AttributeInfo;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.ClassUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.validator.constraints.NotBlank;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.restdocs.RestDocumentationContext;
+import org.springframework.restdocs.operation.Operation;
+import org.springframework.restdocs.snippet.Snippet;
+import org.springframework.restdocs.snippet.WriterResolver;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.method.HandlerMethod;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.beans.PropertyDescriptor;
+import java.io.IOException;
+import java.io.Writer;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class AttributeSnippet implements Snippet {
+
+    private Logger logger = LoggerFactory.getLogger(AttributeSnippet.class);
+
+    public List<String> attributeTable(Class<?> clazz) {
+        if (ClassUtils.isPrimitiveOrWrapper(clazz) || Date.class.isAssignableFrom(clazz) || String.class.isAssignableFrom(clazz)) {
+            return Lists.newArrayList();
+        }
+
+        PropertyDescriptor[] pds = BeanUtils.getPropertyDescriptors(clazz);
+        List<Attribute> attributes = Lists.newArrayList();
+
+        for (PropertyDescriptor pd : pds) {
+            if (pd.getReadMethod() != null && pd.getReadMethod().isAnnotationPresent(AttributeInfo.class)) {
+                Attribute attribute = createAttribute(clazz, pd);
+                attributes.add(attribute);
+            } else {
+                logger.info("AttributeInfo is not present, ignore the property : {}", pd.getName());
+            }
+        }
+
+        List<Attribute> collect = attributes.stream()
+                .filter(a -> !a.isIgnore())
+                .filter(a -> !ArrayUtils.contains(new String[]{"class"}, a.getPath()))
+                .collect(Collectors.toList());
+
+        return toTable(collect, showTableName(clazz));
+    }
+
+    private Attribute createAttribute(Class<?> responseClass, PropertyDescriptor pd) {
+        String propertyName = pd.getName();
+
+        AttributeInfo.Type type = AttributeInfo.Type.valueOf(pd.getPropertyType());
+        Attribute attribute = new Attribute();
+        attribute.setPath(propertyName);
+        if (AttributeInfo.Type.UNKNOWN == type || AttributeInfo.Type.OBJECT == type) {
+            attribute.setType(pd.getPropertyType().getSimpleName());
+        } else {
+            attribute.setType(type.name());
+        }
+
+        Column column = pd.getReadMethod().getAnnotation(Column.class);
+        JoinColumn joinColumn = pd.getReadMethod().getAnnotation(JoinColumn.class);
+
+        if (responseClass.getAnnotation(Table.class) != null && responseClass.getAnnotation(Table.class).name() != null) {
+            if (column != null) {
+                attribute.setColumn(responseClass.getAnnotation(Table.class).name() + "." + column.name());
+            }
+
+            if (joinColumn != null) {
+                attribute.setColumn(responseClass.getAnnotation(Table.class).name() + "." + joinColumn.name());
+            }
+        }
+
+        try {
+            boolean required = isRequired(responseClass, pd, propertyName);
+            attribute.setRequired(required);
+        } catch (Exception e) {
+            attribute.setRequired(false);
+        }
+
+        Optional<AttributeInfo> infoOptional = findAttributeInfo(responseClass, pd, propertyName);
+
+        if (infoOptional.isPresent()) {
+            AttributeInfo info = infoOptional.get();
+            attribute.setDefaultValue(info.defaultValue());
+            attribute.setFormat(info.format());
+            attribute.setDescription(info.description());
+            attribute.setIgnore(info.ignore());
+
+            if (StringUtils.isNotBlank(info.path())) {
+                attribute.setPath(info.path());
+            }
+            if (AttributeInfo.Type.UNKNOWN != info.type()) {
+                attribute.setPath(info.type().name());
+            }
+        }
+        return attribute;
+    }
+
+    protected List<String> toTable(List<Attribute> collect, boolean showColumn) {
+        List<String> results = Lists.newArrayList();
+        results.add("|===");
+
+        StringBuilder header = new StringBuilder();
+        header.append("| Field");
+        header.append("| Required");
+        header.append("| Type");
+        header.append("| default");
+        header.append("| Format");
+        if (showColumn) {
+            header.append("| Column");
+        }
+        header.append("| Description");
+        results.add(header.toString());
+
+
+        for (Attribute a : collect) {
+            results.add("");
+            results.add("| " + a.getPath());
+            results.add("| " + (a.isRequired() ? "*" : ""));
+            results.add("| " + a.getType().toLowerCase());
+            results.add("| " + a.getDefaultValue());
+            results.add("| " + a.getFormat());
+            if (showColumn) {
+                results.add("| " + a.getColumn());
+            }
+            results.add("| " + a.getDescription());
+
+        }
+        results.add("|===");
+        return results;
+    }
+
+    private Optional<AttributeInfo> findAttributeInfo(Class<?> responseClass, PropertyDescriptor pd, String propertyName) {
+        Optional<AttributeInfo> infoOptional = Optional.empty();
+        try {
+            Field field = responseClass.getField(propertyName);
+            infoOptional = Optional.ofNullable(field.getAnnotation(AttributeInfo.class));
+        } catch (NoSuchFieldException e) {
+
+        }
+
+        if (!infoOptional.isPresent()) {
+            try {
+                Field declaredField = responseClass.getDeclaredField(propertyName);
+                infoOptional = Optional.ofNullable(declaredField.getAnnotation(AttributeInfo.class));
+            } catch (NoSuchFieldException e) {
+
+            }
+        }
+        if (!infoOptional.isPresent()) {
+            infoOptional = Optional.ofNullable(pd.getReadMethod().getAnnotation(AttributeInfo.class));
+        }
+        return infoOptional;
+    }
+
+    private boolean isRequired(Class<?> responseClass, PropertyDescriptor pd, String propertyName) {
+        boolean required = false;
+        try {
+            Field field = responseClass.getField(propertyName);
+            if (field.isAnnotationPresent(NotNull.class)) {
+                required = true;
+            }
+            if (field.isAnnotationPresent(NotBlank.class)) {
+                required = true;
+            }
+        } catch (NoSuchFieldException e) {
+
+        }
+
+        try {
+            Field declaredField = responseClass.getDeclaredField(propertyName);
+            if (declaredField.isAnnotationPresent(NotNull.class)) {
+                required = true;
+            }
+            if (declaredField.isAnnotationPresent(NotBlank.class)) {
+                required = true;
+            }
+        } catch (NoSuchFieldException e) {
+
+        }
+
+        if (pd.getReadMethod().isAnnotationPresent(NotNull.class)) {
+            required = true;
+        }
+        if (pd.getReadMethod().isAnnotationPresent(NotBlank.class)) {
+            required = true;
+        }
+
+        return required;
+    }
+
+
+    @Override
+    public void document(Operation operation) throws IOException {
+        MvcResult mvcResult = (MvcResult) operation.getAttributes().get("org.springframework.test.web.servlet.MockMvc.MVC_RESULT_ATTRIBUTE");
+        HandlerMethod hm = (HandlerMethod) mvcResult.getHandler();
+        Method method = hm.getMethod();
+        Parameter[] parameters = method.getParameters();
+        write(operation, method.getReturnType());
+        for (Parameter parameter : parameters) {
+            write(operation, parameter.getType());
+        }
+    }
+
+    private void write(Operation operation, Class parameterType) throws IOException {
+        List<String> strings = attributeTable(parameterType);
+
+
+        RestDocumentationContext context = (RestDocumentationContext) operation
+                .getAttributes().get(RestDocumentationContext.class.getName());
+        WriterResolver writerResolver = (WriterResolver) operation.getAttributes().get(
+                WriterResolver.class.getName());
+        try (Writer writer = writerResolver.resolve(operation.getName(), parameterType.getSimpleName(), context)) {
+            String lines = Joiner.on("\n").join(strings);
+            writer.append(lines);
+        }
+    }
+
+    private boolean showTableName(Class<?> parameterType) {
+        boolean showColumn;
+        showColumn = !(parameterType.getAnnotation(Table.class) == null || parameterType.getAnnotation(Table.class).name() == null);
+        return showColumn;
+    }
+}
